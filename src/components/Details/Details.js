1aab8192f9fef15292c692bbdcfab51ff105a0db
alberto <medina.v@pm.me>
2021-10-14 20:22:24 +0100

    fixed a loeading in deatils component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -23,49 +23,57 @@ export default function Details() {
 
   return (
     <div className='movie--section'>
-      <div className='description'>
-        <div className='movie--title'>{data.Title}</div>
-        <div className='movie--rating'>
-          <span>
-            IMDb Rating <FaStar color='#ff9e00' /> : {data.imdbRating}
-          </span>
-          <span>
-            IMDb Votes <FaThumbsUp color='#fafafa' /> : {data.imdbVotes}
-          </span>
-          <span>
-            Runtime <FaFilm color='#BFD5D6' /> : {data.Runtime}
-          </span>
-          <span>
-            Year <FaCalendar color='#FFE5B4' /> : {data.Year}
-          </span>
-        </div>
-        <div className='movie--plot'>{data.Plot}</div>
-        <div className='movie--info'>
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+    {/* the condition belows fixes a bug causes a delays the info below
+    if theres data it render the divs right aways other it will display loading */}
+      {Object.keys(data).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <>
+          <div className='description'>
+            <div className='movie--title'>{data.Title}</div>
+            <div className='movie--rating'>
+              <span>
+                IMDb Rating <FaStar color='#ff9e00' /> : {data.imdbRating}
+              </span>
+              <span>
+                IMDb Votes <FaThumbsUp color='#fafafa' /> : {data.imdbVotes}
+              </span>
+              <span>
+                Runtime <FaFilm color='#BFD5D6' /> : {data.Runtime}
+              </span>
+              <span>
+                Year <FaCalendar color='#FFE5B4' /> : {data.Year}
+              </span>
+            </div>
+            <div className='movie--plot'>{data.Plot}</div>
+            <div className='movie--info'>
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Generes</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Language</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className='description--img'>
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Generes</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Language</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className='description--img'>
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </div>
   );
 }
