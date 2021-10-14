18c6467e2295025cdd2280433e2912b626b27c87
alberto <medina.v@pm.me>
2021-10-14 14:05:47 +0100

    configured redux store

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,18 +2,22 @@ import React, { useEffect } from 'react';
 import Listing from '../Listing/Listing';
 import omdbAPI from '../../common/API/API';
 import { APIKey } from '../../common/API/APIKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 export default function Home() {
+  const movieSearch = 'harry';
+  const dispatch = useDispatch();
+
   useEffect(() => {
     // if [], run once when commponent loads, and don't run it again.
-    const movieSearch = 'harry';
     const getData = async () => {
       const response = await omdbAPI
         .get(`?apikey=${APIKey}&s=${movieSearch}&type=movie`)
         .catch((error) => {
           console.log('error', error);
         });
-      console.log('api res', response);
+      dispatch(addMovies(response.data));
     };
     getData();
   }, []);
--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllMovies } from '../../features/movies/movieSlice';
+import Card from '../Card/Card';
 
 export default function Listing() {
+  const movies = useSelector(getAllMovies);
+  let renderedMovies = '';
+
+  renderedMovies =
+    movies.Response === 'True' ? (
+      movies.Search.map((movie, index) => {
+        <Card key={index} data={movie} />;
+      })
+    ) : (
+      <div className='movie--error'>
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+  console.log(movies);
   return (
-    <div>
-      Movie listing
+    <div className='movie--wrapper'>
+      <div className='movie--list'>
+        <h2>Movies</h2>
+        <div className='movie--container'>{renderedMovies}</div>
+      </div>
     </div>
-  )
+  );
 }
