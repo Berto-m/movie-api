9f372bc4457143b8e29f903d14caf5895150da22
alberto <medina.v@pm.me>
2021-10-14 12:46:25 +0100

    exploring omdb data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,11 +5,18 @@ import { APIKey } from '../../common/API/APIKey';
 
 export default function Home() {
   useEffect(() => {
+    // if [], run once when commponent loads, and don't run it again.
     const movieSearch = 'harry';
     const getData = async () => {
-      const response = await omdbAPI.get(`?apikey=${APIKey}&s=${movieSearch}`);
+      const response = await omdbAPI
+        .get(`?apikey=${APIKey}&s=${movieSearch}&type=movie`)
+        .catch((error) => {
+          console.log('error', error);
+        });
+      console.log('api res', response);
     };
-  });
+    getData();
+  }, []);
 
   return (
     <div>
